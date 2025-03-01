ea58f50fb600c0c7766d76d4c27f1c77001b292c
Nghialailaptrinh <datdao420@gmail.com>
2025-03-01 10:36:29 +0700

    Update learn.cpp

--- learn.cpp
+++ learn.cpp
@@ -6,7 +6,7 @@ int  tim_nhi_phan(int n,int l,int r,int a[100]){
 if(l>r)return -1;
 int m=l+(r-l)/2;
 if(a[m]==n)return m;
-else if(a[m]>n)return tim_nhi_phan(n,m+1,r,a);
+else if(a[m]<n)return tim_nhi_phan(n,m+1,r,a);
 else return tim_nhi_phan (n,l,m-1,a);
 }
 
