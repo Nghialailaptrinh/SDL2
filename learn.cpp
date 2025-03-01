64945550e491fdbcfb3b3fd723d76ec7dea47c74
Nghialailaptrinh <datdao420@gmail.com>
2025-03-01 10:30:38 +0700

    Update learn.cpp

--- learn.cpp
+++ learn.cpp
@@ -1,16 +1,21 @@
  #include <iostream>
 
 using namespace std;
-int a[100];
-int  tim_nhi_phan(int n,int l,int r,int a[]){
+
+int  tim_nhi_phan(int n,int l,int r,int a[100]){
 if(l>r)return -1;
 int m=l+(r-l)/2;
 if(a[m]==n)return m;
-else if(a[m]>)
+else if(a[m]>n)return tim_nhi_phan(n,m+1,r,a);
+else return tim_nhi_phan (n,l,m-1,a);
 }
 
 int main(){
-
+int n;cin>>n;
+int m;cin>>m;
+int a[m];
+for( int i=0;i<m;i++)cin>>a[i];
+cout<<tim_nhi_phan(n,0,m-1,a);
 
 
 }
